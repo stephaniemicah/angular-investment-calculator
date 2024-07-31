51cb6ba93bccde6202c35202015c3aa46b214e04
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-07-31 14:29:27 +0800

    Complete App

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
 import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 
 export class AppComponent {
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
   onCalculateInvestmentResults( data: InvestmentInput) {
     //destructor
     const {initialInvestment, annualInvestment, duration, expectedReturn} = data;
@@ -33,6 +42,6 @@ export class AppComponent {
       });
     }
 
-    return console.log(annualData);
+    this.resultsData = annualData;
   }
 }
