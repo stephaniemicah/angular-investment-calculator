7d5a25d9609f6460d2338884b9bbab60207b9a9e
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-07-31 11:48:26 +0800

    Calculate annual investment data

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+
+export class AppComponent {
+  onCalculateInvestmentResults( data: {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }) {
+    //destructor
+    const {initialInvestment, annualInvestment, duration, expectedReturn} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return console.log(annualData);
+  }
+}
